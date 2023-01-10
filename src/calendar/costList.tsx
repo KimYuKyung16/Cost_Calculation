b4c07ed5bf36ed04adef6ee61d6df8919caee898
kyk <kyg0954@naver.com>
2023-01-10 23:49:06 +0900

    infinite scroll test

--- src/calendar/costList.tsx
+++ src/calendar/costList.tsx
@@ -14,11 +14,32 @@ import styled from "styled-components"; // styled in js
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  /* background-color: #d2d2d2; */
   width: 100vw;
   height: 100vh;
   padding: 1% 3%;
+  box-sizing: border-box;  
+  /* overflow: auto;  */
+`
+
+const Header = styled.div`
+  height: 10%;
+`
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* width: 100%; */
+  height: 90%;
   box-sizing: border-box;
+  overflow: auto; // 화면을 넘어가면 스크롤이 되도록
+  background-color: aquamarine;
+  /* -ms-overflow-style: none;
+  scrollbar-width: none; */
+
+  // 스크롤바 안보이게 하기
+  /* &::-webkit-scrollbar {
+    display: none;
+  } */
 `
 
 const Main__List = styled.table`
@@ -70,6 +91,37 @@ function CostList(props: Props) {
   const costList = useAppSelector(state => state.cost); // 멤버 리스트
   console.log(costList.list)
 
+  const options: any = {
+    root: document.querySelector('.main'), // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+    rootMargin: '10px', // rootMargin을 '10px 10px 10px 10px'로 설정
+    threshold: [0, 0.5, 1] // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
+  }
+
+  let observer = new IntersectionObserver(loading, options);
+
+  // IntersectionObserver 생성
+  const io = new IntersectionObserver((entries, observer) => {
+    // IntersectionObserverEntry 객체 리스트와 observer 본인(self)를 받음
+    // 동작을 원하는 것 작성
+    entries.forEach(entry => {
+      // entry와 observer 출력
+      console.log('entry:', entry);
+      console.log('observer:', observer);
+    })
+  }, options)
+
+
+  function loading() {
+    console.log('loading중');
+  }
+
+  io.observe(options.root);
+
+  console.log(io);
+
+
+
+
   function costListPrint() {
     axios.get('http://localhost:6001/costlist', {
       params: {
@@ -89,23 +141,27 @@ function CostList(props: Props) {
 
   return(
     <Container>
-      <h1>비용 정산 리스트</h1>
-      
-      <Main__List>
-        <tbody>
-          {
-            costList.list.map((x, index) => {
-              return(
-                <tr>
-                  <td className='test1'>{x.title}</td>
-                  <td className='test2'>{x.payer}</td>
-                  <td className='test3'>{x.cost} 원</td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </Main__List>
+      <Header>
+        <h1>비용 정산 리스트</h1>
+      </Header>
+
+      <Main className='main'>
+        <Main__List>
+          <tbody>
+            {
+              costList.list.map((x, index) => {
+                return(
+                  <tr>
+                    <td className='test1'>{x.title}</td>
+                    <td className='test2'>{x.payer}</td>
+                    <td className='test3'>{x.cost} 원</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Main__List>
+      </Main>
     </Container>
   )
 }
